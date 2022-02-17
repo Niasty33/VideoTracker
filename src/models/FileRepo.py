c32b77b261319e5cd8018df6410d0b2b90064b15
Niasty <marie.sanson33@gmail.com>
2022-02-17 16:19:23 +0100

    maj

--- src/models/FileRepo.py
+++ src/models/FileRepo.py
@@ -1,18 +1,19 @@
-import point
+from point import *
+
+
 class FileRepo(object):
     def __init__(self,tab=[]):
         self.tab = tab
 
     def export2CSV(self):
         file = open('tests/points.csv','w')
-        for k in len(self.tab):
+        for k in range(len(self.tab)):
             line = f"{self.tab[k].x} ; {self.tab[k].y}\n"
             file.write(line)
         file.close()
 
 
 if __name__ == "__main__":
-    
-    f = FileRepo(colonne_x)
-    print(f)
+    t = randomPoints(8)
+    f = FileRepo(t)
     f.export2CSV()
--- src/models/point.py
+++ src/models/point.py
@@ -1,6 +1,7 @@
 import math
+from random import random
 
-class Point():
+class Point:
     def __init__(self,x = 0,y = 0):
         self.x = x
         self.y = y
@@ -19,9 +20,20 @@ def distance(A, B):
     dy = abs(A.y - B.y)
     return math.sqrt(dx**2 + dy**2)
 
+def randomPoints(n:int):
+    tab = [0]*n
+    for k in range(n):
+        px = random()
+        py= random()
+        tab[k] = Point(px,py)
+    return tab
+
+
 if __name__ == "__main__":
     origin = Point()
     p = Point(3,4)
     print(p)
     print(distance(origin, p))
+    t = randomPoints(8)
+    print(t)
 
