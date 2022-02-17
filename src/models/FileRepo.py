d671530cc2e2a303a37b86c21fe9e0d7d6702b8d
Niasty <marie.sanson33@gmail.com>
2022-02-17 16:52:04 +0100

    maj

--- src/models/FileRepo.py
+++ src/models/FileRepo.py
@@ -5,7 +5,7 @@ class FileRepo(object):
         self.tab = tab
 
     def export2CSV(self):
-        file = open('tests/points.csv','w')
+        file = open('../tests/points.csv','w')
         for k in range(len(self.tab)):
             line = f"{self.tab[k].x} ; {self.tab[k].y}\n"
             file.write(line)
